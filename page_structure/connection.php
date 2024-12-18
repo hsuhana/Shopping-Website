db6963b223099bf34265b78b0efb1aa5767e3745
tracy <200548417@student.georgianc.on.ca>
2024-12-18 11:26:48 -0500

    add connection

--- page_structure/connection.php
+++ page_structure/connection.php
@@ -1,8 +1,14 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "phpAssignment2";
+    // $servername = "localhost";
+    // $username = "root";
+    // $password = "";
+    // $database = "phpAssignment2";
+
+    $servername = "sql208.infinityfree.com";
+    $username = "if0_37938189";
+    $password = "ap0dxiLcNKcq";
+    $database = "if0_37938189_shoppingweb";
+
 
     $conn = new mysqli($servername, $username, $password, $database);
     
